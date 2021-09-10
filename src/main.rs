4698106971d4e42dadf277c2085f9e42f9137ca8
Zachary Fogg <me@zfo.gg>
2021-09-10 19:42:31 -0400

    reorganized into files and stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod block;
 mod wallet;
+use block::Block;
+use wallet::{Wallet, WalletAddress};
 
 use chrono::{DateTime, Utc};
 
@@ -7,13 +9,13 @@ fn main() {
     let utc_now: DateTime<Utc> = Utc::now();
     println!("now: {:#?}", utc_now);
 
-    let mut wallet_id = wallet::WalletAddress::new();
+    let mut wallet_id = WalletAddress::new();
     wallet_id.push_str("149826a38f759df945dd3f91f47392483e9289d349b67b13f08874894b623499");
-    let miner = wallet::Wallet { id: wallet_id };
+    let miner = Wallet { id: wallet_id };
     println!("miner: {:#?}", miner);
 
-    let b1: block::Block = block::Block::create_block(None, miner.clone());
+    let b1: Block = Block::create_block(None, miner.clone());
     println!("block1: {:#?}", b1);
-    let b2: block::Block = block::Block::create_block(Some(Box::new(b1)), miner.clone());
+    let b2: Block = Block::create_block(Some(Box::new(b1)), miner.clone());
     println!("block2: {:#?}", b2);
 }
